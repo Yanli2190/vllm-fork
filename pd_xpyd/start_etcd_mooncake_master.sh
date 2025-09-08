2cbc720605e62a87874e01645b6e01aec3341b9b
Zhang, Yanli L <yanli.l.zhang@intel.com>
2025-09-08 11:18:16 +0800

    Update mooncake log dir
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/start_etcd_mooncake_master.sh
+++ pd_xpyd/start_etcd_mooncake_master.sh
@@ -27,13 +27,13 @@ fi
 
 etcd --listen-client-urls http://0.0.0.0:2379 \
      --advertise-client-urls http://localhost:2379 \
-     > /tmp/etcd.log 2>&1 &
+     > /var/log/etcd.log 2>&1 &
 
 if [ "$BENCHMARK_MODE" == "1" ]; then
-  #mooncake_master -max_threads 64 -port 50001 --v=1 > /tmp/mooncake_master.log 2>&1 &
-  mooncake_master -max_threads 64 -port 50001 > /tmp/mooncake_master.log 2>&1 &
+  #mooncake_master -max_threads 64 -port 50001 --v=1 > /var/log/mooncake_master.log 2>&1 &
+  mooncake_master -max_threads 64 -port 50001 > /var/log/mooncake_master.log 2>&1 &
 else
-  #mooncake_master -enable_gc true -max_threads 64 -port 50001 --v=1 > /tmp/mooncake_master.log 2>&1 &
-  mooncake_master -enable_gc true -max_threads 64 -port 50001 > /tmp/mooncake_master.log 2>&1 &
+  #mooncake_master -enable_gc true -max_threads 64 -port 50001 --v=1 > /var/log/mooncake_master.log 2>&1 &
+  mooncake_master -enable_gc true -max_threads 64 -port 50001 > /var/log/mooncake_master.log 2>&1 &
 fi
 
