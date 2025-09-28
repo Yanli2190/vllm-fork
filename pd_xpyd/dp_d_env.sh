dcf9bdf734b1869ab9ce7dbb9a8dd08336a8dd66
Zhang, Yanli L <yanli.l.zhang@intel.com>
2025-09-28 14:34:20 +0800

    Enable fp8 inc with 1.23 docker/3.5 INC
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/dp_d_env.sh
+++ pd_xpyd/dp_d_env.sh
@@ -71,9 +71,9 @@ export VLLM_EP_SIZE=16
 #export VLLM_SKIP_WARMUP=True
 #export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/pd_d_cache,false,131072
 if [ "$INC_FP8" -eq 1 ]; then
-  export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww38_pd_fp8_inc_d_cache,false,13107200
+  export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww38_pd_fp8_inc_d_cache,false,131072,false
 else
-  export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww38_pd_bf16_d_cache,false,131072
+  export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww38_pd_bf16_d_cache,false,131072,false
 fi
 
 # MoE settings
