6f51cdadd10633c57043676537923e74ca2a31bf
Zhang, Yanli L <yanli.l.zhang@intel.com>
2025-11-28 17:31:40 +0800

    bug fix
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/start_etcd_mooncake_master.sh
+++ pd_xpyd/start_etcd_mooncake_master.sh
@@ -8,9 +8,9 @@ pkill -f etcd
 sleep 5s
 
 # Define commands as arrays
-#ETCD_CMD=(rm -rf default.etcd && etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://localhost:2379)
+#ETCD_CMD=(etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://localhost:2379)
 #MOON_CMD=(mooncake_master -max_threads 64 -port 50001 -eviction_high_watermark_ratio 0.8 -eviction_ratio 0.2)
-ETCD_CMD=(rm -rf default.etcd && etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://localhost:2379 --debug)
+ETCD_CMD=(etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://localhost:2379 --debug)
 MOON_CMD=(mooncake_master -max_threads 64 -port 50001 -eviction_high_watermark_ratio 0.8 -eviction_ratio 0.2 -v=1)
 
 # Check if XPYD_LOG is set
@@ -20,6 +20,7 @@ if [ -n "$XPYD_LOG" ]; then
     # Run etcd with logging
     ETCD_LOG="$XPYD_LOG/etcd_${timestamp}.log"
     echo "Starting etcd, logging to $ETCD_LOG..."
+    rm -rf default.etcd
     "${ETCD_CMD[@]}" > "$ETCD_LOG" 2>&1 &
 
     # Run mooncake_master with logging
@@ -29,6 +30,7 @@ if [ -n "$XPYD_LOG" ]; then
 else
     # Run without logging
     echo "XPYD_LOG not set, running without logging..."
+    rm -rf default.etcd
     "${ETCD_CMD[@]}" > /dev/null 2>&1 &
     "${MOON_CMD[@]}" > /dev/null 2>&1 &
 fi
